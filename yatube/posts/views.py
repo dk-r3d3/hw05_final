2b2dba937b38074b45329b1fbf5fa0700e3fdc87
Дмитрий Коротеев <blessdevil@yandex.ru>
2022-02-24 23:43:07 +0700

    First

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -132,7 +132,12 @@ def follow_index(request):
 def profile_follow(request, username):
     """Подписка на автора."""
     author = get_object_or_404(User, username=username)
-    Follow.objects.get_or_create(user=request.user, author=author)
+    is_exist = Follow.objects.filter(user=request.user, author=author).exists()
+    if author != request.user and not is_exist:
+        Follow.objects.get_or_create(
+            user=request.user,
+            author=author
+        )
     return redirect('posts:profile', username=username)
 
 
